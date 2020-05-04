2e9d5befd1c28cac038a097416364e3bb8c79441
fajara <fajarakurniawan@gmail.com>
2020-05-04 22:39:04 +0700

    halfway done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 use std::env;
+use std::process;
+pub mod lib;
+// use minigrep;
+// use minigrep::Config;
+
+// use minigrep::Config;
+// use minigrep::Config;
+// use minigrep::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    
+    let config = lib::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = lib::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
+
