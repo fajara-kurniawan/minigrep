d9c9e7d8213d30ee1070e63c9e230b9dea0611a0
fajara-kurniawan <fajarakurniawan@gmail.com>
2020-05-05 21:10:24 +0700

    end of tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
 use std::env;
 use std::process;
 pub mod lib;
-// use minigrep;
-// use minigrep::Config;
-
-// use minigrep::Config;
-// use minigrep::Config;
-// use minigrep::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = lib::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.filename);
 
     if let Err(e) = lib::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
 
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+
+//     let config = Config::new(&args).unwrap_or_else(|err| {
+//         eprintln!("Problem parsing arguments: {}", err);
+//         process::exit(1);
+//     });
+
+//     if let Err(e) = minigrep::run(config) {
+//         eprintln!("Application error: {}", e);
+
+//         process::exit(1);
+//     }
+// }
